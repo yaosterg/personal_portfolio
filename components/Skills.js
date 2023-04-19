36779a167070974b5bba040fd6076e00f8294cc1
Yao Guan <yaozongguan@gmail.com>
2023-04-18 23:37:42 -0400

    carousel images

--- components/Skills.js
+++ components/Skills.js
@@ -7,77 +7,67 @@ export default function Skills() {
   useEffect(() => {
     setTimeout(() => {
       carousel > 10 ? setCarousel(0) : setCarousel(carousel + 1);
-      console.log(carousel);
-    }, 3000);
+    }, 2000);
     return () => {};
   }, [carousel]);
   return (
     <div id={styles.carouselbox}>
       <section className={styles.carousel}>
         <ul className={styles.carousel__list}>
-          <li className={styles.carousel__item}>
+          <li
+            className={styles.carousel__item}
+            id={carousel === 0 ? styles.data_active : null}
+          >
             <div className={styles.carousel__image}>
               <img
                 className={styles.carousel__item__img}
-                src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=687&q=80"
+                src="https://res.cloudinary.com/thewebmaster/image/upload/c_scale,f_auto,q_auto:best,w_516/images/html/html5.png"
                 width="480"
                 height="720"
               />
             </div>
             <div className={styles.carousel__contents}>
-              <h2 className={styles.user__name}>Anthony Lee</h2>
-              <h3 className={styles.user__title}>
-                Archetypal Professor of Recognition
-              </h3>
+              <h2 className={styles.user__name}>HTML</h2>
+              <h3 className={styles.user__title}>Language</h3>
             </div>
           </li>
-          <li className={styles.carousel__item}>
+          <li
+            className={styles.carousel__item}
+            id={carousel === 1 ? styles.data_active : null}
+          >
             <div className={styles.carousel__image}>
               <img
                 className={styles.carousel__item__img}
-                src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?fit=crop&w=688&q=80"
+                src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
                 width="480"
                 height="720"
               />
             </div>
             <div className={styles.carousel__contents}>
-              <h2 className={styles.user__name}>Alicia Chevalier</h2>
-              <h3 className={styles.user__title}>
-                Corporate Usability Analyst
-              </h3>
-            </div>
-          </li>
-          <li className={styles.carousel__item}>
-            <div className={styles.carousel__image}>
-              <img
-                className={styles.carousel__item__img}
-                src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?fit=crop&w=687&q=80"
-                width="480"
-                height="720"
-              />
-            </div>
-            <div className={styles.carousel__contents}>
-              <h2 className={styles.user__name}>Nate Boucher</h2>
-              <h3 className={styles.user__title}>Customer Impact Officer</h3>
-            </div>
-          </li>
-          <li className={styles.carousel__item}>
-            <div className={styles.carousel__image}>
-              <img
-                className={styles.carousel__item__img}
-                src="https://images.unsplash.com/photo-1614204424926-196a80bf0be8?fit=crop&w=687&q=80"
-                width="480"
-                height="720"
-              />
-            </div>
-            <div className={styles.carousel__contents}>
-              <h2 className={styles.user__name}>Leah Harris</h2>
-              <h3 className={styles.user__title}>Designer and Bandit </h3>
+              <h2 className={styles.user__name}>Javascript</h2>
+              <h3 className={styles.user__title}>Language</h3>
             </div>
           </li>
           <li
             className={styles.carousel__item}
             id={carousel === 2 ? styles.data_active : null}
+          >
+            <div className={styles.carousel__image}>
+              <img
+                className={styles.carousel__item__img}
+                src="https://w0.peakpx.com/wallpaper/209/515/HD-wallpaper-python-logo-coding-kod-kodlama-code-mobile-lock-programming.jpg"
+                width="480"
+                height="720"
+              />
+            </div>
+            <div className={styles.carousel__contents}>
+              <h2 className={styles.user__name}>Python</h2>
+              <h3 className={styles.user__title}>Language</h3>
+            </div>
+          </li>
+          <li
+            className={styles.carousel__item}
+            id={carousel === 3 ? styles.data_active : null}
           >
             <div className={styles.carousel__image}>
               <img
@@ -89,118 +79,149 @@ export default function Skills() {
             </div>
             <div className={styles.carousel__contents}>
               <h2 className={styles.user__name}>React.js</h2>
-              <h3 className={styles.user__title}>Technology</h3>
+              <h3 className={styles.user__title}>Library</h3>
             </div>
           </li>
-          <li className={styles.carousel__item}>
+          <li
+            className={styles.carousel__item}
+            id={carousel === 4 ? styles.data_active : null}
+          >
             <div className={styles.carousel__image}>
               <img
                 className={styles.carousel__item__img}
-                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?fit=crop&w=688&q=80"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1200px-Postgresql_elephant.svg.png"
                 width="480"
                 height="720"
               />
             </div>
             <div className={styles.carousel__contents}>
-              <h2 className={styles.user__name}>Gal Gadot</h2>
-              <h3 className={styles.user__title}>
-                Acting Designer and Consultant{" "}
-              </h3>
+              <h2 className={styles.user__name}>PostgreSQL</h2>
+              <h3 className={styles.user__title}>Database</h3>
             </div>
           </li>
-          <li className={styles.carousel__item}>
+
+          <li
+            className={styles.carousel__item}
+            id={carousel === 5 ? styles.data_active : null}
+          >
             <div className={styles.carousel__image}>
               <img
                 className={styles.carousel__item__img}
-                src="https://images.unsplash.com/photo-1590086782792-42dd2350140d?fit=crop&w=687&q=80"
+                src="https://raw.githubusercontent.com/reduxjs/redux/master/logo/logo.png"
                 width="480"
                 height="720"
               />
             </div>
             <div className={styles.carousel__contents}>
-              <h2 className={styles.user__name}>Albert Sørensen</h2>
-              <h3 className={styles.user__title}>
-                Neural Big Shot of Anticipation{" "}
-              </h3>
+              <h2 className={styles.user__name}>Redux</h2>
+              <h3 className={styles.user__title}>Library</h3>
             </div>
           </li>
-          <li className={styles.carousel__item}>
+          <li
+            className={styles.carousel__item}
+            id={carousel === 6 ? styles.data_active : null}
+          >
             <div className={styles.carousel__image}>
               <img
                 className={styles.carousel__item__img}
-                src="https://images.unsplash.com/photo-1553514029-1318c9127859?fit=crop&w=764&q=80"
+                src="https://images.ctfassets.net/c63hsprlvlya/IacLLeOBR5WCvdCPqKuff/6860b5cc464c4f54703a2befa3f706b4/nextjs3.webp"
                 width="480"
                 height="720"
               />
             </div>
             <div className={styles.carousel__contents}>
-              <h2 className={styles.user__name}>Candice Marchand</h2>
-              <h3 className={styles.user__title}>
-                Mindful Realist of Motion Laws{" "}
-              </h3>
+              <h2 className={styles.user__name}>Next.js</h2>
+              <h3 className={styles.user__title}>Framework</h3>
             </div>
           </li>
-          <li className={styles.carousel__item}>
+          <li
+            className={styles.carousel__item}
+            id={carousel === 7 ? styles.data_active : null}
+          >
             <div className={styles.carousel__image}>
               <img
                 className={styles.carousel__item__img}
-                src="https://images.unsplash.com/photo-1596813362035-3edcff0c2487?fit=crop&w=735&q=80"
+                src="https://colorlib.com/cdn-cgi/image/width=1600,height=948,fit=crop,quality=80,format=auto,onerror=redirect,metadata=none/wp-content/uploads/sites/2/nodejs-frameworks.png"
                 width="480"
                 height="720"
               />
             </div>
             <div className={styles.carousel__contents}>
-              <h2 className={styles.user__name}>Jennifer Salazar</h2>
-              <h3 className={styles.user__title}>Design Habitué </h3>
+              <h2 className={styles.user__name}>Node.js</h2>
+              <h3 className={styles.user__title}>Runtime Environment</h3>
             </div>
           </li>
-          <li className={styles.carousel__item}>
+          <li
+            className={styles.carousel__item}
+            id={carousel === 8 ? styles.data_active : null}
+          >
             <div className={styles.carousel__image}>
               <img
                 className={styles.carousel__item__img}
-                src="https://images.unsplash.com/photo-1552374196-c4e7ffc6e126?fit=crop&w=687&q=80"
+                src="https://www.vectorlogo.zone/logos/expressjs/expressjs-ar21.png"
                 width="480"
                 height="720"
               />
             </div>
             <div className={styles.carousel__contents}>
-              <h2 className={styles.user__name}>Antonin Dufour</h2>
-              <h3 className={styles.user__title}>Human-Centered Designer</h3>
+              <h2 className={styles.user__name}>Express</h2>
+              <h3 className={styles.user__title}>Framework </h3>
             </div>
           </li>
-          <li className={styles.carousel__item}>
+          <li
+            className={styles.carousel__item}
+            id={carousel === 9 ? styles.data_active : null}
+          >
             <div className={styles.carousel__image}>
               <img
                 className={styles.carousel__item__img}
-                src="https://images.unsplash.com/photo-1530785602389-07594beb8b73?fit=crop&w=687&q=80"
+                src="https://f9s9t9d6.rocketcdn.me/wp-content/uploads/2020/04/Artboard-11-1024x724.png"
                 width="480"
                 height="720"
               />
             </div>
             <div className={styles.carousel__contents}>
-              <h2 className={styles.user__name}>Melissa Simon</h2>
-              <h3 className={styles.user__title}>
-                International Infrastructure Analyst
-              </h3>
+              <h2 className={styles.user__name}>CSS</h2>
+              <h3 className={styles.user__title}>Language</h3>
             </div>
           </li>
-          <li className={styles.carousel__item}>
+          <li
+            className={styles.carousel__item}
+            id={carousel === 10 ? styles.data_active : null}
+          >
             <div className={styles.carousel__image}>
               <img
                 className={styles.carousel__item__img}
-                src="https://images.unsplash.com/photo-1560250097-0b93528c311a?fit=crop&w=687&q=80"
+                src="https://cdn-media-1.freecodecamp.org/images/0*CPTNvq87xG-sUGdx.png"
                 width="480"
                 height="720"
               />
             </div>
             <div className={styles.carousel__contents}>
-              <h2 className={styles.user__name}>Brandon Murray</h2>
-              <h3 className={styles.user__title}>Central Functionality VP</h3>
+              <h2 className={styles.user__name}>Firebase</h2>
+              <h3 className={styles.user__title}>Cloud</h3>
+            </div>
+          </li>
+          <li
+            className={styles.carousel__item}
+            id={carousel === 11 ? styles.data_active : null}
+          >
+            <div className={styles.carousel__image}>
+              <img
+                className={styles.carousel__item__img}
+                src="https://cdn-images-1.medium.com/v2/resize:fit:2050/1*BKssrX-qEmyN6YaRNTvNlw.png"
+                width="480"
+                height="720"
+              />
+            </div>
+            <div className={styles.carousel__contents}>
+              <h2 className={styles.user__name}>Material UI</h2>
+              <h3 className={styles.user__title}>Component Library</h3>
             </div>
           </li>
         </ul>
 
-        <div className={styles.carousel__nav}>
+        {/* <div className={styles.carousel__nav}>
           <Button className="prev" variant="primary">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -244,7 +265,7 @@ export default function Skills() {
               ></path>
             </svg>
           </Button>
-        </div>
+        </div> */}
       </section>
     </div>
   );
