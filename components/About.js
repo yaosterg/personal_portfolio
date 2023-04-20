208afec3ea8d8c8ab83088750c7207d8f5a251fa
Yao Guan <yaozongguan@gmail.com>
2023-04-19 23:32:21 -0400

    fixing

--- components/About.js
+++ components/About.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "../styles/Aboutme.module.css";
 import Link from "next/link";
 
-export default function Aboutme() {
+export default function About() {
   return (
     <div className={styles.aboutmediv}>
       <div className={styles.aboutmeinfo}>
